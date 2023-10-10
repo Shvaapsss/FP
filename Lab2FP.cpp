292807acc0b8d7d0017812f7b4a463ec6b5b0159
Shvaapsss <nekita20045544@gmail.com>
2023-10-11 00:43:41 +0300

    123123

--- Lab2FP.cpp
+++ Lab2FP.cpp
@@ -1,27 +1,41 @@
 #include <iostream>
 using namespace std;
 int main() {
-    const int size=5;
+    const int size = 100;
     int a[size];
-    for (int i=0;i<size;i++) {
-        cin>>a[i];
+    for (int i = 0; i < size; i++) {
+        cin >> a[i];
     }
-    cout<<"1:"<<endl;
-    for (int i=0;i<size;i++) {
-        cout<<a[i]<<" ";
-    }
-    cout<<endl;
-    int pos_i=size-1;
-    int neg_i=0;
 
-    while (neg_i<=pos_i) {
-        if (a[pos_i] < 0) {
-            swap(a[neg_i], a[pos_i]);
-            neg_i++;
+    int negativeNumbers[size];
+    int positiveNumbers[size];
+    int negIndex = 0;
+    int posIndex = 0;
+
+    cout << "1:" << endl;
+    for (int i = 0; i < size; i++) {
+        cout << a[i] << " ";
+        if (a[i] < 0) {
+            negativeNumbers[negIndex] = a[i];
+            negIndex++;
         } else {
-            pos_i--;
+            positiveNumbers[posIndex] = a[i];
+            posIndex++;
         }
     }
+    cout << endl;
+
+    int index = 0;
+    for (int i = 0; i < negIndex; i++) {
+        a[index] = negativeNumbers[i];
+        index++;
+    }
+
+    for (int i = 0; i < posIndex; i++) {
+        a[index] = positiveNumbers[i];
+        index++;
+    }
+
     cout<<"2:"<<endl;
     for(int i=0;i<size;i++) {
         cout<<a[i]<<" ";
@@ -29,9 +43,6 @@ int main() {
     cout<<endl;
     return 0;
 }
-//
-// Created by nicki on 10/10/2023.
-//
 //
 // Created by nicki on 10/10/2023.
 //
